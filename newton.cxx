6ff66e7a67e933c1293a67c2d525b539f99f4032
Klim Petrov <klpetr@pc11.pool.tp1.uni-duesseldorf.de>
2015-11-04 18:52:24 +0100

    Newton lab3

--- newton.cxx
+++ newton.cxx
@@ -1,13 +1,25 @@
 #include <iostream>
 #include <complex>
-
+using namespace std;
 int main(){
-
-  // Example how to declare a single complex variable:
-  complex<double> c;
-
-  // d = 1 + 2i 
-  complex<double> d = complex<double>(1.0, 2.0); 
-
+  
+  complex<double> v;
+  int j;
+  for(double im = -2; im<=2; im+=0.01){
+    for(double re = -2; re<=2; re+=0.01){
+     complex<double> zn = complex<double>(re,im);
+     
+     for(j=0;j<400; j++){
+       v=zn;
+       zn = zn - (zn*zn*zn-1.0)/(3.0*zn*zn);
+       if (abs(v-zn)  <=1e-8 ) break;
+       
+     }
+     cout << re << "   "<< im << "   " <<  j << endl;
+    }
+  }
+  
+  
+  
   return 0;
 }
